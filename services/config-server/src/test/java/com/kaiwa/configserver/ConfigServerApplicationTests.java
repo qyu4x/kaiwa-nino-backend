d24a3183ac74081839113b2a0a88a805f718fb95
Ariq Khoiri <qq.khoiri@gmail.com>
2024-07-10 23:47:52 +0700

    feat: config config-server & discovery service

---
+++ services/config-server/src/test/java/com/kaiwa/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaiwa.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
